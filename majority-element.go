0d52da8e3bef4cf6de7278f69850335ceca88b0a
vikas <vikas@stackbox.xyz>
2021-05-19 11:52:05 +0530

    O(1) space majority element

--- majority-element.go
+++ majority-element.go
@@ -7,7 +7,27 @@ func main() {
 	fmt.Println(majorityElement(nums)) //should print 2
 }
 
+//using space O(1)
 func majorityElement(nums []int) int {
+	count, majorityEl := 0, nums[0]
+
+	for k, v := range nums {
+		if v == majorityEl {
+			count++
+		} else {
+			count--
+		}
+
+		if count == 0 {
+			majorityEl = nums[k+1]
+		}
+	}
+
+	return majorityEl
+}
+
+//using hash map
+func majorityElement_v1(nums []int) int {
 	count := make(map[int]int)
 	for _, v := range nums {
 		count[v] += 1
